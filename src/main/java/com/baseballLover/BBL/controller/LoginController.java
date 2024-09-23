365b9da0cda8e924568e8a6bed7b77d512fcceb0
joyjelly <noop0728@naver.com>
2024-09-23 22:47:20 +0900

    commit~2247

--- src/main/java/com/baseballLover/BBL/controller/LoginController.java
+++ src/main/java/com/baseballLover/BBL/controller/LoginController.java
@@ -1,8 +1,12 @@
 package com.baseballLover.BBL.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class LoginController {
-
+    @RequestMapping("/")
+    private String goToIndex(){
+        return "login";
+    }
 }
