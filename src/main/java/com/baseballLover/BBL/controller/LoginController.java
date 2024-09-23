b7e85921e8fdaa2f834c9b6b5d724e82ecd6f66f
joyjelly <noop0728@naver.com>
2024-09-23 21:13:56 +0900

    Initial commit

---
+++ src/main/java/com/baseballLover/BBL/controller/LoginController.java
@@ -0,0 +1,8 @@
+package com.baseballLover.BBL.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class LoginController {
+
+}
---
+++ src/test/java/com/baseballLover/BBL/BblApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baseballLover.BBL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BblApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
