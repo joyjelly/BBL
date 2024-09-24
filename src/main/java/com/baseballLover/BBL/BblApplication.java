fee0b554edcdda6060040fedf1c9d018d1ed3fc7
joyjelly <noop0728@naver.com>
2024-09-24 19:02:29 +0900

    헤이

---
+++ src/main/java/com/baseballLover/BBL/BblApplication.java
@@ -0,0 +1,13 @@
+package com.baseballLover.BBL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BblApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BblApplication.class, args);
+	}
+
+}
